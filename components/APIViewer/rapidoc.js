d61314189ce3f45f7cf38a9d562bc4bca496df5a
Shawn Yap <shawn.yap@bigcommerce.com>
2022-12-16 11:57:41 -0600

    Better illustration of demo

--- components/APIViewer/rapidoc.js
+++ components/APIViewer/rapidoc.js
@@ -1,30 +1,36 @@
 import { useTheme } from "nextra-theme-docs";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const RapidocViewer = () => {
+  const [componentLoaded, setComponentLoaded] = useState(false);
   const { resolvedTheme } = useTheme();
 
   useEffect(() => {
-    import("rapidoc");
+    import("rapidoc").then(() => {
+      setComponentLoaded(true);
+    });
   }, []);
 
   return (
-    <rapi-doc
-      spec-url="https://raw.githubusercontent.com/bigcommerce/api-specs/master/reference/pages.v3.yml"
-      render-style="view"
-      font-size="large"
-      class="bg-white"
-      show-header="false"
-      allow-search="false"
-      allow-advanced-search="false"
-      theme={resolvedTheme}
-      // onLoaded={() => {
-      //   const hash = window.location.hash
-      //   if (hash !== '') {
-      //     scroller.scrollTo(hash.slice(1), { offset: -100 })
-      //   }
-      // }}
-    />
+    componentLoaded && (
+      <rapi-doc
+        spec-url="https://raw.githubusercontent.com/bigcommerce/api-specs/master/reference/pages.v3.yml"
+        render-style="view"
+        font-size="large"
+        class="bg-white"
+        show-header="false"
+        allow-search="false"
+        allow-advanced-search="false"
+        theme={resolvedTheme}
+        // onLoaded={() => {
+        //   const hash = window.location.hash
+        //   if (hash !== '') {
+        //     scroller.scrollTo(hash.slice(1), { offset: -100 })
+        //   }
+        // }}
+        suppressHydrationWarning
+      />
+    )
   );
 };
 
