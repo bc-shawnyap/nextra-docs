49d813fcd608af7da51d25e829a4d16513b44a07
Shawn Yap <shawn.yap@bigcommerce.com>
2022-12-16 17:26:57 -0600

    Include variants to Card

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,14 +1,28 @@
 import Link from "next/link";
 
+type CardVariants = "small" | "large" | "default";
+
 interface CardProps {
   title: string;
   description: string;
   href: string;
+  variant?: CardVariants;
 }
 
-export default function Card({ title, description, href }: CardProps) {
+const cardVariants = {
+  small: "col-span-12 md:col-span-4",
+  large: "col-span-12",
+  default: "col-span-12 md:col-span-6",
+};
+
+export default function Card({
+  title,
+  description,
+  href,
+  variant = "default",
+}: CardProps) {
   return (
-    <Link className="col-span-12 md:col-span-6" href={href}>
+    <Link className={cardVariants[variant]} href={href}>
       <div className="relative h-full group cursor-pointer overflow-hidden border rounded-lg text-left border-scale-500 hover:border-scale-700  dark:bg-scale-300 transition">
         <div className="px-8 py-8 relative flex flex-col h-full gap-6">
           <div className="flex items-center gap-3">
