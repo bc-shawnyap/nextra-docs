09f287ff42794e8417c33ddd045780ec3fd1e8b4
Shawn Yap <shawn.yap@bigcommerce.com>
2022-12-16 17:25:16 -0600

    Include tailwind in markdowns

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
+  darkMode: "class",
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx,md,mdx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
