c08e39cb84d43475a3099d86745296403d311a12
Shawn Yap <shawn.yap@bigcommerce.com>
2022-12-16 13:47:37 -0600

    Add tailwind + sass

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import "nextra-theme-docs/style.css";
-import "@styles/global.css";
+import "@styles/global.scss";
+import "@styles/tailwind.scss"
 
 export default function Nextra({ Component, pageProps }) {
   return <Component {...pageProps} />;
