3337f7e727d2d921ee51134a8181d6b80138be6b
Shawn Yap <shawn.yap@bigcommerce.com>
2022-12-20 13:23:54 -0600

    Prep app for custom layouts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
+import { Fragment } from "react";
+
+import type { AppProps } from "next/app";
+import type { Page } from "types/page";
+
 import "nextra-theme-docs/style.css";
 import "@styles/global.scss";
-import "@styles/tailwind.scss"
+import "@styles/tailwind.scss";
 
-export default function Nextra({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+type Props = AppProps & {
+  Component: Page;
+};
+
+export default function Nextra({ Component, pageProps }: Props) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+  const Layout = Component.layout ?? Fragment;
+
+  return <Layout>{getLayout(<Component {...pageProps} />)}</Layout>;
 }
---
+++ pages/_document.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/dracula-prism@2.1.13/dist/css/dracula-prism.min.css"
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
