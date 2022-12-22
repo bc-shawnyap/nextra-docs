197ee1490a404d0588abef02fa1f29967cb96a9b
Shawn Yap <shawn.yap@bigcommerce.com>
2022-12-21 18:29:16 -0600

    Add auth to increase gh api limit

--- lib/github.ts
+++ lib/github.ts
@@ -3,6 +3,14 @@ const config = {
   owner: "bigcommerce",
 } as const;
 
+const fetchConfig = {
+  headers: {
+    accept: "application/vnd.github+json",
+    Authorization: process.env.GH_API_AUTH_TOKEN,
+    "X-GitHub-Api-Version": "2022-11-28",
+  },
+};
+
 export const getGithubRepoContents = async ({
   repositoryName,
   path,
@@ -12,12 +20,7 @@ export const getGithubRepoContents = async ({
 }) => {
   const repoResponse = await fetch(
     `${GH_API_BASE_URL}/repos/${config.owner}/${repositoryName}/contents/${path}`,
-    {
-      headers: {
-        accept: "application/vnd.github+json",
-        "X-GitHub-Api-Version": "2022-11-28",
-      },
-    }
+    fetchConfig
   );
 
   return await repoResponse.json();
@@ -31,7 +34,8 @@ export const getGitTreeWithSHA1 = async ({
   SHA1: string;
 }) => {
   const repoResponse = await fetch(
-    `${GH_API_BASE_URL}/repos/${config.owner}/${repositoryName}/git/trees/${SHA1}?recursive=true`
+    `${GH_API_BASE_URL}/repos/${config.owner}/${repositoryName}/git/trees/${SHA1}?recursive=true`,
+    fetchConfig
   );
 
   return await repoResponse.json();
