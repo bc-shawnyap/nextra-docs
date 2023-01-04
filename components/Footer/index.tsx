f783068e34127ec03a970fc266e23a3e2da8df1d
Shawn Yap <shawn.yap@bigcommerce.com>
2023-01-04 13:00:50 -0600

    Cleanup components

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
   return (
-    <div className="bg-slate-900 dark:bg-black h-[100px] w-full text-white">
-      Fake Footer
-    </div>
+    <footer className="bg-slate-900 dark:bg-black px-4 py-6 w-full text-white mt-auto">
+      <span className="text-sm text-gray-200">
+        ©{new Date().getFullYear()} BigCommerce Pty. Ltd. Shopping Cart Software
+      </span>
+    </footer>
   );
 }
--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -3,12 +3,12 @@ import CustomThemeSwitch from "@components/Switch/CustomThemeSwitch";
 
 export default function Navbar() {
   return (
-    <div className="py-4 w-full dark:text-white dark:bg-black">
-      <nav className="flex w-full">
+    <div className="p-4 w-full dark:text-white dark:bg-black">
+      <nav className="flex w-full items-center">
         <div className="flex">
           <Logo />
         </div>
-        <div className="ml-auto px-4">
+        <div className="ml-auto">
           <CustomThemeSwitch />
         </div>
       </nav>
