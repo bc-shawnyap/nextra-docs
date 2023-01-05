d05034540398e4b29f8bbe728a9d747540592e83
Shawn Yap <shawn.yap@bigcommerce.com>
2023-01-05 12:49:45 -0600

    Remove hyphen from title

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -13,7 +13,9 @@ const cardVariants = {
   small: "col-span-12 md:col-span-4",
   large: "col-span-12",
   default: "col-span-12 md:col-span-6",
-};
+} as const;
+
+const hyphenRegex: RegExp = /-/g;
 
 export default function Card({
   title,
@@ -26,7 +28,9 @@ export default function Card({
       <div className="relative h-full group cursor-pointer overflow-hidden border rounded-lg text-left border-slate-500 hover:border-slate-700 hover:shadow-lg dark:hover:shadow-none dark:border-slate-700 dark:bg-slate-800 dark:hover:bg-slate-600 transition">
         <div className="px-8 py-8 relative flex flex-col h-full gap-6">
           <div className="flex items-center gap-3">
-            <h5 className="text-base font-bold">{title}</h5>
+            <h5 className="text-sm font-bold uppercase">
+              {title.replace(hyphenRegex, " ")}
+            </h5>
           </div>
           <span>{description}</span>
           <span className="mt-auto text-[#0d52ff] dark:text-[#01ebdb]">
